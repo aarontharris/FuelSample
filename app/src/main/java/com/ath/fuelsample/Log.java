e78afe9f2c065c1bdfa60454708da0b42bdd8c72
Aaron T Harris <aarontharris@gmail.com>
2016-07-16 17:13:32 -0700

    better logging for debugging frag scope support

--- app/src/main/java/com/ath/fuelsample/Log.java
+++ app/src/main/java/com/ath/fuelsample/Log.java
@@ -1,14 +1,26 @@
 package com.ath.fuelsample;
 
+import com.ath.fuel.FLog;
+
 public class Log {
     private static final String LOG_TAG = "FUELSAMPLE";
 
     public static void d( String format, Object...args ) {
-        android.util.Log.d( LOG_TAG, String.format( format, args ) );
+        android.util.Log.d( LOG_TAG, format( format, args ) );
     }
 
     public static void e( Exception e, String format, Object...args ) {
-        android.util.Log.e( LOG_TAG, String.format( format, args ), e );
+        android.util.Log.e( LOG_TAG, format( format, args ), e );
     }
 
+    private static String format( String format, Object...args ) {
+        return defaultPrefix() + String.format( format, args );
+    }
+
+    private static String defaultPrefix() {
+        StackTraceElement elem = FLog.findStackElem( Log.class );
+        String callingClassName = FLog.getSimpleName( elem );
+        int lineNo = elem.getLineNumber();
+        return callingClassName + "[" + lineNo + "]: ";
+    }
 }
