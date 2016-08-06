9b798122e345942ac8b959f7909c2a8040849bd9
Aaron T Harris <aarontharris@gmail.com>
2016-08-06 08:40:45 -0700

    reduce tests

--- app/src/main/java/com/ath/fuelsample/MainActivity.java
+++ app/src/main/java/com/ath/fuelsample/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
 		Log.d( "MyClass1: %s", mMyClass1.get() );
 		Log.d( "MyClass2: %s", mMyClass2.get() );
 
-		int iterations = 20000;
+		int iterations = 100;
 		timeTest1( iterations ); // Singletons
 		timeTest2( iterations ); // Pojos
 		timeTest3( iterations ); // Mixed test 1/2 Pojos and 1/2 Singletons
