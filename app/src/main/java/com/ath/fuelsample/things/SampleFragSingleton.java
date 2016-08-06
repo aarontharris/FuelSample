ee1cbed730c159b53b285fac958576b782096014
Aaron T Harris <aarontharris@gmail.com>
2016-08-06 09:16:26 -0700

    frag singleton within frag singleton, no leaks

--- app/src/main/java/com/ath/fuelsample/things/SampleFragSingleton.java
+++ app/src/main/java/com/ath/fuelsample/things/SampleFragSingleton.java
@@ -6,9 +6,11 @@ import com.ath.fuel.Lazy;
 @FragmentSingleton
 public class SampleFragSingleton {
 	private final Lazy<MemoryEater> mMemoryEater = Lazy.attain( this, MemoryEater.class );
+	private final Lazy<SampleFragSingleton2> mSampleFragSingleton2 = Lazy.attain( this, SampleFragSingleton2.class );
 
 	public String doStuff() {
 		mMemoryEater.get();
+		mSampleFragSingleton2.get().helloWorld();
 		return String.format( "An Activity should not be able to inject me %s", this );
 	}
 
